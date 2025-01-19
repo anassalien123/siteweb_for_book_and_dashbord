18b1c45cb800e231dbf90915914a87218ab11cc9
Anass Cheab <anassalien123@gmail.com>
2025-01-19 16:03:53 +0100

    Initialy commit

---
+++ js/admin_script.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('.header .navbar');
+let accountBox = document.querySelector('.header .account-box');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+}
+document.querySelector('#user-btn').onclick = () => {
+    accountBox.classList.toggle('active');
+}
